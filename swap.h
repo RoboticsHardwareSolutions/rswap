9783346a42c371431696d7ef7e9965d8387e19ae
DocPillman <doc.pillman@icloud.com>
2020-10-19 16:34:03 +0300

    inital

---
+++ swap.h
@@ -0,0 +1,14 @@
+#ifndef __SWAP_H
+#define __SWAP_H
+
+#include "stdint.h"
+
+uint16_t swap_uint16(uint16_t val);
+
+int16_t swap_int16(int16_t val);
+
+uint32_t swap_uint32(uint32_t val);
+
+int32_t swap_int32(int32_t val);
+
+#endif
